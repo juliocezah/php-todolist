d6bb861c58b3fb2bb7ed15359553fb354e235e58
juliocezah <julim.cdo@gmail.com>
2015-11-10 22:31:57 +0000

    Added /api/tasks

--- api/index.php
+++ api/index.php
@@ -8,10 +8,21 @@ $app->get('/', function() use ($app){
     echo "Welcome to REST API";
 });
 
-//http:.../api/index.php/hello/Julio
-$app->get('/hello/:name', function($name) use ($app){
-    echo "Hi $name, welcome to the REST API's";
+$app->get('/tasks', function() use ($app){
+    
+    $tasks = getTasks();
+    
+    echo json_encode($tasks);
 });
 
+//TODO move it to a DAO class
+function getTasks(){
+     $tasks = array(
+        array('id' => 1, 'description' => 'Learn REST', 'done' => false), //posição 0 do vetor tasks []
+        array('id' => 2, 'description' => 'Learn JavaScript', 'done' => false), //posição 1 do vetor tasks []
+    );
+    
+    return $tasks;
+}
 $app->run();
 ?>
