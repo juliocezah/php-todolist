d2d8316b6e38e5dd624024656590d14f35536367
juliocezah <julim.cdo@gmail.com>
2015-11-10 22:03:10 +0000

    Added Apache config

--- api/index.php
+++ api/index.php
@@ -8,6 +8,7 @@ $app->get('/', function() use ($app){
     echo "Welcome to REST API";
 });
 
+//http:.../api/index.php/hello/Julio
 $app->get('/hello/:name', function($name) use ($app){
     echo "Hi $name, welcome to the REST API's";
 });
