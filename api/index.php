24421dffd70e4f82a5c87ddeeb60c71cb21b8b02
juliocezah <julim.cdo@gmail.com>
2015-11-16 21:44:30 +0000

    Added coments

--- api/index.php
+++ api/index.php
@@ -9,8 +9,27 @@ $app->get('/', function() use ($app){
     echo "Welcome to REST API";
 });
 
-//get a task by id
-//http://url(...)/api/tasks
+/*
+HTTP GET http://url(...)/api/tasks
+RESPONSE 200 OK
+[
+  {
+    "id": 1,
+    "description": "Learn REST",
+    "done": false
+  },
+  {
+    "id": 2,
+    "description": "Learn JavaScript",
+    "done": true
+  },
+  {
+    "id": 3,
+    "description": "Learn English",
+    "done": false
+  }
+]
+*/
 $app->get('/tasks', function() use ($app){
     $tasks = getTasks();
     //Define what kind is this response / Define qual é o tipo de resposta
@@ -18,6 +37,17 @@ $app->get('/tasks', function() use ($app){
     echo json_encode($tasks);
 });
 
+/*
+HTTP GET http://url(...)/api/tasks/1
+RESPONSE 200 OK
+{
+    "id": 1,
+    "description": "Learn REST",
+    "done": false
+}
+
+RESPONSE 204 NO CONTENT
+*/
 $app->get('/tasks/:id', function($id) use ( $app ){
    $tasks = getTasks();
    $index = array_search($id, array_column($tasks, 'id'));
@@ -32,6 +62,16 @@ $app->get('/tasks/:id', function($id) use ( $app ){
    }
 });
 
+/*
+HTTP POST http://url(...)/api/tasks
+REQUEST Body
+{
+  "description": "Learn REST",
+}
+
+RESPONSE 200 OK Body
+Learn REST added
+*/
 $app->post('/tasks', function() use ( $app ) {
     $taskJson = $app->request()->getBody();
     $task = json_decode($taskJson);
